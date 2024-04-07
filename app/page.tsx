3c93eb8189b05d39665c1851f14926aaac764e7e
Andrii Yusyp <serafimsanvol@gmail.com>
2024-04-07 22:10:30 +0300

    Move types to proper places

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import Card from "@/components/home/card";
-import { DEPLOY_URL } from "@/lib/constants";
-import { Github, Twitter } from "@/components/shared/icons";
-import WebVitals from "@/components/home/web-vitals";
-import ComponentGrid from "@/components/home/component-grid";
-import Image from "next/image";
-import { nFormatter } from "@/lib/utils";
-
 export default async function Home() {
   return (
     <div className="hero min-h-[90vh]">
